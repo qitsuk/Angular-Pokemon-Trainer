2e11299b197770fe03bc075ad41091007fd52f21
Kasper <kasper.andreasen@gmail.com>
2022-02-08 09:36:50 +0100

    Testing Heroku

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/<angular-pokemon-trainer>'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/<app-name>/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
